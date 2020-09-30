49e1c62053afebc3bce3790633ac06c56cfa0343
ankush789 <ankushsharma.4771@gmail.com>
2020-09-30 11:35:37 +0530

    RxJs Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
-
+import { switchMap } from 'rxjs/operators';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -20,8 +20,20 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
+    //Using observable to fetch the clicked dish from the getDish() method in dish Service by passing id obtained using activatedRoute. 
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+
+    // const id = this.route.snapshot.params['id'];
+    // this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
+  }
+
+  //SETTING NEXT AND PREVIOUS DISH
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack(): void {
