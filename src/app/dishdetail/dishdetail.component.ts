88bb563b4c3d52f84a6c811a32dc797f15f9f735
ankush789 <ankushsharma.4771@gmail.com>
2020-10-03 11:22:26 +0530

    Saving Changes to Server

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -10,12 +10,11 @@ import { Comment } from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
-
   dish: Dish;
-  dishIds: string [];
+  dishIds: string[];
   prev: string;
   next: string;
   commentForm: FormGroup;
@@ -24,50 +23,80 @@ export class DishdetailComponent implements OnInit {
   authorName: string;
   comments: string;
   errMess: String;
-   
+  dishcopy: Dish;
+
   @ViewChild('commentform') commentFormDirective;
   formErrors = {
-   'name': '',
-    'comments': ''
+    name: '',
+    comments: '',
   };
 
   validationMessages = {
-    'name': {
-      'required': 'Name is required.',
-      'minlength': 'Name must be at least 2 characters long.',
-      'maxlength': 'Name cannot be more than 30 characters long.'
+    name: {
+      required: 'Name is required.',
+      minlength: 'Name must be at least 2 characters long.',
+      maxlength: 'Name cannot be more than 30 characters long.',
     },
-    'comments': {
-      'required': 'Comment is required.',
-      'minlength': 'Comment must be at least 10 characters long.',
-      'maxlength': 'Comment cannot be more than 200 characters long.'
-    }
+    comments: {
+      required: 'Comment is required.',
+      minlength: 'Comment must be at least 10 characters long.',
+      maxlength: 'Comment cannot be more than 200 characters long.',
+    },
   };
 
-  constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location,
-    public fb: FormBuilder, @Inject('BaseURL') private baseURL) { this.createCommentForm(); }
+  constructor(
+    private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    public fb: FormBuilder,
+    @Inject('BaseURL') private baseURL
+  ) {
+    this.createCommentForm();
+  }
 
   ngOnInit() {
-    //Using observable to fetch the clicked dish from the getDish() method in dish Service by passing id obtained using activatedRoute. 
-    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds, errmess => this.errMess = <any>errmess);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    //Using observable to fetch the clicked dish from the getDish() method in dish Service by passing id obtained using activatedRoute.
+    this.dishservice.getDishIds().subscribe(
+      (dishIds) => (this.dishIds = dishIds),
+      (errmess) => (this.errMess = <any>errmess)
+    );
+    this.route.params.pipe( switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe((dish) => {
+        this.dish = dish;
+        this.dishcopy = dish;
+        this.setPrevNext(dish.id);
+      });
 
     // const id = this.route.snapshot.params['id'];
     // this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
   }
 
   //Comment Form Submission function
-  createCommentForm(){
+  createCommentForm() {
     this.commentForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
-      comments: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(200)]],
-      rating: ["5"]
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ],
+      ],
+      comment: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(200),
+        ],
+      ],
+      rating: ['5'],
     });
 
-    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.commentForm.valueChanges.subscribe((data) =>
+      this.onValueChanged(data)
+    );
     this.onValueChanged();
-
   }
 
   onValueChanged(data?: any) {
@@ -94,33 +123,43 @@ export class DishdetailComponent implements OnInit {
   //SETTING NEXT AND PREVIOUS DISH
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
-    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+    this.prev = this.dishIds[
+      (this.dishIds.length + index - 1) % this.dishIds.length
+    ];
+    this.next = this.dishIds[
+      (this.dishIds.length + index + 1) % this.dishIds.length
+    ];
   }
 
   goBack(): void {
     this.location.back();
   }
-  onSubmit(){
-
-    var d = new Date();
-    this.date = d.toISOString();
-    this.comment.date = this.date;
-
-    //ASSIGNING VALUES OF COMMENT FORM TO THE JSON OBJECT COMMENT WHICH IS TO BE PUSHED 
-    this.comment.author = this.commentForm.value.name;
-    this.comment.comment = this.commentForm.value.comments;
-    this.comment.rating = this.commentForm.value.rating;
+  onSubmit() {
+    //ASSIGNING VALUES OF COMMENT FORM TO THE JSON OBJECT COMMENT WHICH IS TO BE PUSHED
+    this.comment = this.commentForm.value;
+   
+    this.comment.date = new Date().toISOString();
 
     //Pushing new Json object into comments array of dishes
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
 
+    
+    this.dishservice.putDish(this.dishcopy).subscribe(
+      (dish) => {
+        this.dish = dish;
+        this.dishcopy = dish;
+      },
+      (errmess) => {
+        this.dish = null;
+        this.dishcopy = null;
+        this.errMess = <any>errmess;
+      }
+    );
+    this.commentFormDirective.resetForm();
     this.commentForm.reset({
       name: '',
       comments: '',
-      rating: 5
+      rating: 5,
     });
-    this.commentFormDirective.resetForm();
   }
-
 }
